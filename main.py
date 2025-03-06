6b1dbf2dd838d31b24e44dbeb2fb0f6b0d2f68bd
Yuhan Wu <m38109@gmail.com>
2025-03-06 15:01:41 +0800

    add tested tournament_selection, parents_selection and crossover function

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def initialize_population(size, grid):
         new_grid = copy.deepcopy(grid)
 
         for row in new_grid:
-            existing_letters = set(row) - {' '} # Get existing letters in the row
+            existing_letters = set(row) - {''} # Get existing letters in the row
             missing_letters = list(letter - existing_letters)  # Find missing letters
             random.shuffle(missing_letters)  # Shuffle missing letters randomly
 
@@ -58,34 +58,60 @@ def count_column_conflicts(grid):
 
 def count_subgrid_conflicts(grid):
     """Compute the number of subgrid conflicts"""
+    count_conflicts = 0
+    subgrid_size = 2
 
+    for start_row in [0,2]:
+        for start_col in [0,2]:
+            letter_counts = {}
 
-
-
-
-
-
+            for i in range(start_row,start_row+subgrid_size):
+                for j in range(start_col,start_col+subgrid_size):
+                    letter = grid[i][j]
+                    if letter:
+                        if letter in letter_counts:
+                            count_conflicts += 1
+                        else:
+                            letter_counts[letter] = 1
     return count_conflicts
 
+#select parents with tournament method
+def tournament_selection(population, tournament_size):
+    best_individual = None
+    best_fitness = float('inf') # the best solution is the one with the lowest fitness
+
+    for _ in range(tournament_size):
+        candidate = random.choice(population)
+        candidate_fitness = fitness(candidate)
+        if candidate_fitness < best_fitness:
+            best_individual = candidate
+            best_fitness = candidate_fitness
+    return best_individual
+
+def parent_selection(population, tournament_size):
+    """Select different individual as parents"""
+    parent1 = tournament_selection(population, tournament_size)
+
+    while True:
+        parent2 = tournament_selection(population, tournament_size)
+        if parent2 != parent1:
+            break
+    return parent1, parent2
 
 
-initial_grid = [['B', '', '', ''],
-                ['', '', '', ''],
-                ['', 'B', '', ''],
-                ['A', '', '', 'B']]
+def crossover(parent1, parent2, initial_grid):
+    """Crossover between two parents """
+    crossover_point = random.randint(1,3)
+    child = copy.deepcopy(parent1)
 
-populations = initialize_population(3, initial_grid)
-populations_with_conflicts = []
-for idx, pop in enumerate(populations):
-    row_conflicts = count_row_conflicts(pop)
-    column_conflicts = count_column_conflicts(pop)
-    populations_with_conflicts.append((idx + 1, pop, row_conflicts, column_conflicts))
+    for i in range(crossover_point,4):
+        for j in range(4):
+            if initial_grid[i][j] == ' ':
+                child[i][j]= parent2[i][j]
+    return child
 
-# Display results
-output_data = []
-for pop_id, pop, row_conflicts, column_conflicts in populations_with_conflicts:
-    output_data.append({"Population": pop_id, "Row Conflicts": row_conflicts, "Column Conflicts": column_conflicts, "Population:": pop})
 
-if output_data:
-    for row in output_data:
-        print(row)
+
+
+
+
